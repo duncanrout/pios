e37512689fbfab9172db1b62837c36adb0a50ed2
duncanrout <duncanrout@yahoo.com>
2021-02-02 17:00:54 -0600

    Update

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,25 @@
+#include <stdio.h>
+
+extern long __bss_start;
+extern long __bss_end;
+int i;
 
 
 
-void kernel_main() {
+void clear_bss(){
 
-    while(1){
-    }
+	char *begin_bss = (char *) &__bss_start;
+	char *end_bss = (char *) &__bss_end;
+
+	for(; begin_bss < end_bss; begin_bss++) {
+		*begin_bss = 0;
+	}
 }
+
+void kernel_main(){
+	while(1){
+	}
+}
+
+
+
