30f5e05997be04514b8504e8e6101286412d389b
duncanrout <duncanrout@yahoo.com>
2021-02-09 14:25:08 -0600

    Update, still having trouble debuggng using "Make" commands

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-extern int __bss_start,__bss_end;
+extern char __bss_start,__bss_end,__bss_size;
+int global;
 
-void clear_bss();
-
-int temp;
-
-
-void kernel_main(){
-	clear_bss();
-	while (1){
+void clear_bss() {
+	char * bssStart = &__bss_start;
+	char * bssEnd = &__bss_end;
+	int bssSize = bssEnd - bssStart;
+	for (int i=0; i<bssSize; i++){
+		bssStart[i] = 0;
 	}
 }
 
-void clear_bss(){
-	(&__bss_start)[0] = 0x0c;
-	int i = 0;
-	while ((&__bss_start)+i != &__bss_end){
-		(&__bss_start)[i] = 0;
-		i++;
+void kernel_main() {
+	clear_bss();
+	while(1) {
 	}
 }
 
